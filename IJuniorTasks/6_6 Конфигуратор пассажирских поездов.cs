a2c4bbf7ab1fc5e256bedc81ff7fd977da637bb9
Attos <attosius@gmail.com>
2025-12-22 23:22:57 +0300

    Add Coliseum stub

---
+++ IJuniorTasks/6_6 Конфигуратор пассажирских поездов.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IJuniorTasks66
+{
+    internal class Program66
+    {
+        static void Main66(string[] args)
+        {
+            var administrator = new Administrator();
+            administrator.Run();
+        }
+    }
+
+    public class Administrator
+    {
+        private const string CommandCreateTrain = "1";
+        private const string CommandExit = "2";
+
+        public void Run()
+        {
+            bool isWork = true;
+
+            var dispatcher = new Dispatcher();
+
+            while (isWork)
+            {
+                Console.Clear();
+                Console.WriteLine($"Созданные поезда:");
+                dispatcher.ShowTrains();
+
+                Console.WriteLine($"\n\nВведите команду:");
+                Console.WriteLine($"{CommandCreateTrain}. Создать поезд");
+                Console.WriteLine($"{CommandExit}. Выход");
+                Console.WriteLine();
+
+                var command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case CommandCreateTrain:
+                        dispatcher.CreateTrain();
+                        break;
+
+                    case CommandExit:
+                        isWork = false;
+                        break;
+
+                    default:
+                        Console.WriteLine($"Некорректная команда!");
+                        break;
+                }
+            }
+        }
+    }
+
+    public class Dispatcher
+    {
+        private const int MinPassengersOnTrain = 200;
+        private const int MaxPassengersOnTrain = 300;
+
+        private readonly List<string> _stations = new() { "Москва", "СПб", "Казань", "Сочи", "Екатеринбург" };
+        private readonly List<Train> _trains = new();
+
+        public void CreateTrain()
+        {
+            Console.WriteLine($"Выберите пункт отправления:");
+
+            if (!TryGetStation(out string stationDeparture))
+            {
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine($"Выберите пункт прибытия:");
+
+            if (!TryGetStation(out string stationDestination))
+            {
+                Console.ReadLine();
+                return;
+            }
+
+            if (stationDeparture.Equals(stationDestination, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"Пункт отравления и пункт прибытия одинаковы!");
+                Console.ReadLine();
+                return;
+            }
+
+            var passengersCount = GetPassengers();
+            var train = BuildTrain(stationDeparture, stationDestination, passengersCount);
+            _trains.Add(train);
+            Console.WriteLine($"Создан новый поезд {train.GetInfo()}");
+            Console.ReadLine();
+        }
+
+        public void ShowTrains()
+        {
+            var index = 0;
+
+            foreach (var train in _trains)
+            {
+                Console.WriteLine($"{++index}. {train.GetInfo()}");
+            }
+        }
+
+        private Train BuildTrain(string stationDeparture, string stationDestination, int passengersCount)
+        {
+            var train = new Train(stationDeparture, stationDestination, passengersCount);
+
+            while (passengersCount > 0)
+            {
+                var carriage = train.BuildCarriage();
+                train.AddCarriage(carriage);
+                passengersCount -= carriage.MaxPassengersCount;
+            }
+
+            return train;
+        }
+
+        private int GetPassengers()
+        {
+            Console.WriteLine("Продаем билеты...");
+            var random = new Random();
+            var passengers = random.Next(MinPassengersOnTrain, MaxPassengersOnTrain + 1);
+            Console.WriteLine($"Продано билетов: {passengers}");
+            return passengers;
+        }
+
+        private bool TryGetStation(out string station)
+        {
+            station = default;
+            ShowStations();
+            var stationIndexStr = Console.ReadLine();
+
+            if (!int.TryParse(stationIndexStr, out var stationIndex) || stationIndex < 0 || stationIndex > _stations.Count)
+            {
+                Console.WriteLine($"Некорректный индекс!");
+                return false;
+            }
+
+            station = _stations[stationIndex - 1];
+            return true;
+        }
+
+        private void ShowStations()
+        {
+            var index = 0;
+
+            foreach (var station in _stations)
+            {
+                Console.WriteLine($"{++index}. {station}");
+            }
+        }
+    }
+
+    public class Train
+    {
+        public const int BusinessClassFrequency = 5;
+
+        private readonly List<Carriage> _carriages = new();
+
+        public Train(string stationDeparture, string stationDestination, int passengersCount)
+        {
+            StationDeparture = stationDeparture;
+            StationDestination = stationDestination;
+            PassengersCount = passengersCount;
+        }
+
+        public string StationDeparture { get; }
+        public string StationDestination { get; }
+        public int PassengersCount { get; }
+        public int MaxPassengersCount
+        {
+            get
+            {
+                return _carriages.Sum(carriage => carriage.MaxPassengersCount);
+            }
+        }
+        public int CarriageCount => _carriages.Count;
+
+        public void AddCarriage(Carriage carriage)
+        {
+            _carriages.Add(carriage);
+        }
+
+        public Carriage BuildCarriage()
+        {
+            var isCreateBusiness = CarriageCount + 1 % BusinessClassFrequency == 0;
+            var carriagePassengersCount = isCreateBusiness
+                ? Carriage.BusinessClassCountPassengers
+                : Carriage.EconomyClassCountPassengers;
+            var carriage = new Carriage(carriagePassengersCount);
+            return carriage;
+        }
+
+        public string GetInfo()
+        {
+            return $"{StationDeparture} - {StationDestination}, пассажиров: {PassengersCount} / {MaxPassengersCount}, вагонов: {_carriages.Count}";
+        }
+    }
+
+    public class Carriage
+    {
+        public const int EconomyClassCountPassengers = 50;
+        public const int BusinessClassCountPassengers = 20;
+
+        public Carriage(int maxPassengersCount)
+        {
+            MaxPassengersCount = maxPassengersCount;
+        }
+
+        public int MaxPassengersCount { get; }
+    }
+}
--- IJuniorTasks/Program.cs
+++ IJuniorTasks/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using IJuniorTasks66;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace IJuniorTasks
 {
@@ -15,23 +17,24 @@ namespace IJuniorTasks
 
     public class Administrator
     {
-        private const string CommandShowGoods = "1";
-        private const string CommandBuy = "2";
-        private const string CommandShowPurchases = "3";
-        private const string CommandExit = "4";
+        private const string CommandWelcomeMessage = "1";
+        private const string CommandFight = "2";
+        private const string CommandExit = "3";
 
         public void Run()
         {
             bool isWork = true;
 
-            var store = new Dispatcher();
+            var coliseum = new Coliseum();
 
             while (isWork)
             {
+                Console.Clear();
+                coliseum.ShowWelcomeMessage();
+
                 Console.WriteLine($"\n\nВведите команду:");
-                Console.WriteLine($"{CommandShowGoods}. Показать товары");
-                Console.WriteLine($"{CommandBuy}. Купить товары");
-                Console.WriteLine($"{CommandShowPurchases}. Показать купленные товары");
+                Console.WriteLine($"{CommandWelcomeMessage}. Приветственное сообщение");
+                Console.WriteLine($"{CommandFight}. Посмотреть бой");
                 Console.WriteLine($"{CommandExit}. Выход");
                 Console.WriteLine();
 
@@ -39,18 +42,14 @@ namespace IJuniorTasks
 
                 switch (command)
                 {
-                    case CommandShowGoods:
-                        store.ShowSellersItems();
+                    case CommandWelcomeMessage:
+                        coliseum.ShowWelcomeMessage();
                         break;
 
-                    case CommandBuy:
-                        store.SellItem();
+                    case CommandFight:
+                        coliseum.CreateFight();
                         break;
 
-                    case CommandShowPurchases:
-                        store.ShowBuyerInfo();
-                        break;
-                        
                     case CommandExit:
                         isWork = false;
                         break;
@@ -63,9 +62,231 @@ namespace IJuniorTasks
         }
     }
 
-    public class Dispatcher
+    public class Coliseum
     {
 
+        private List<Fighter> _fighters = new();
+
+        public Coliseum()
+        {
+            _fighters = ArenaDungeons.GetAllFighters();
+        }
+
+        public void ShowWelcomeMessage()
+        {
+            Console.WriteLine("Ave, Caesar, morituri te salutant!");
+        }
+
+        public void CreateFight()
+        {
+            Console.WriteLine("Выберите первого бойца:");
+
+            if (!TryGetFighter(out Fighter firstFighter))
+            {
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine($"Выберите второго бойца:");
+
+            if (!TryGetFighter(out Fighter secondFighter))
+            {
+                Console.ReadLine();
+                return;
+            }
+            Arena arena = new Arena();
+            arena.Fight(firstFighter, secondFighter);
+            Console.ReadLine();
+        }
+
+        public void ShowFighters()
+        {
+            var index = 0;
+
+            foreach (var fighter in _fighters)
+            {
+                Console.WriteLine($"{++index}. {fighter.Name}");    
+            }
+        }
+
+        private bool TryGetFighter(out Fighter fighter)
+        {
+            fighter = default;
+
+            while (fighter == default)
+            {
+                ShowFighters();
+                var fighterIndexStr = Console.ReadLine();
+
+                if (!int.TryParse(fighterIndexStr, out var fighterIndex) || fighterIndex < 0 || fighterIndex > _fighters.Count)
+                {
+                    Console.WriteLine($"Некорректный индекс! Введите число от 1 до {_fighters.Count}");
+                    continue;
+                }
+
+                var currentFighter = (Fighter)_fighters[fighterIndex - 1].Clone();
+
+                Console.WriteLine($"Вы выбрали {currentFighter.Name}! Его характеристики:");
+                currentFighter.ShowInfo();
+
+                Console.WriteLine($"Продолжить с этим бойцом? (y/n)");
+                var answer = Console.ReadLine();
+
+                if (answer.Equals("y", StringComparison.OrdinalIgnoreCase))
+                {
+                    fighter = currentFighter;
+                    return true;
+                }
+
+                Console.WriteLine($"Выберите бойца:");
+            }
+
+            return true;
+        }
     }
-    
+    public class FighterDualAttack : Fighter
+    {
+        public FighterDualAttack(string name, int damage) : base(name, damage)
+        {
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Способность: двойной урон с вероятностью {DualAttackPercent}%");
+        }
+
+        public int DualAttackPercent { get; } = 30;
+
+        public override void Attack(IDamageable damageable)
+        {
+            var damage = Damage * 2;
+
+            damageable.TakeDamage(damage);
+            Console.WriteLine($"{Name} бьет! Двойной удар! Урон: {damage}");
+        }
+        public override object Clone()
+        {
+            return new FighterDualAttack(Name, Damage);
+        }
+    }
+
+    public class Fighter : ICloneable, IDamageable
+    {
+        public Fighter(string name, int damage)
+        {
+            Name = name;
+            Damage = damage;
+        }
+        public string Name { get; } = "Fedor";
+        public int Damage { get; private set; } = 1;
+        public int Health { get; private set; } = 100;
+
+        public bool IsAlive => Health > 0;
+        public bool IsDead => Health <= 0;
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.WriteLine($"Урон: {Damage}");
+        }
+
+        public virtual object Clone()
+        {
+            return new Fighter(Name, Damage);
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"{Name} остаток жизни: {Health}");
+        }
+
+        public virtual void Attack(IDamageable damageable)
+        {
+            Attack(damageable, Damage);
+        }
+
+        public void Attack(IDamageable damageable, int damage)
+        {
+            damageable.TakeDamage(damage);
+            Console.WriteLine($"{Name} бьет! Урон: {damage}");
+        }
+    }
+
+    public interface IDamageable
+    {
+        void TakeDamage(int damage);
+    }
+
+    public class Arena
+    {
+        public void Fight(Fighter firstFighter, Fighter secondFighter)
+        {
+            // SetFirstMove(ref firstFighter, ref secondFighter);
+            var round = 0;
+
+            while (firstFighter.IsAlive && secondFighter.IsAlive)
+            {
+                Console.WriteLine($"Раунд {++round}!");
+                firstFighter.Attack(secondFighter);
+                secondFighter.Attack(firstFighter);
+                Console.WriteLine();
+            }
+
+            if (firstFighter.IsDead && secondFighter.IsDead)
+            {
+                Console.WriteLine($"Ничья!");
+                return;
+            }
+
+            var winner = firstFighter.IsAlive ? firstFighter : secondFighter;
+
+            Console.WriteLine($"Победил {winner}");
+        }
+
+        private static void SetFirstMove(ref Fighter firstFighter, ref Fighter secondFighter)
+        {
+            var changeFirstMove = UserUtils.GenerateRandomBool();
+
+            if (changeFirstMove)
+            {
+                var temp = firstFighter;
+                firstFighter = secondFighter;
+                secondFighter = temp;
+            }
+        }
+    }
+
+    public class UserUtils
+    {
+        private static Random s_random = new Random();
+
+        public static bool GenerateRandomBool()
+        {
+            return s_random.Next(2) == 0;
+        }
+
+        public static int GenerateRandomNumber(int max)
+        {
+            return s_random.Next(max + 1);
+        }
+    }
+
+    public class ArenaDungeons
+    {
+        public static List<Fighter> GetAllFighters()
+        {
+            var list = new List<Fighter>
+            {
+                new FighterDualAttack("Спартак", 10),
+                new Fighter("Коммод", 12),
+                new Fighter("Кавендиш", 12),
+                new Fighter("Максимус", 20),
+                new Fighter("Фламма ", 30),
+            };
+
+            return list;
+        }
+    }
 }
